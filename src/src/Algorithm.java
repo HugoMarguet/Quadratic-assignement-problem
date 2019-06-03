0a1d65ea16e46ee11390bc8348f72708079f944a
hugoMarguet <hugo.marguet@hotmail.fr>
2019-06-03 21:41:22 +0200

    merge class Data and QAP

--- src/src/Algorithm.java
+++ src/src/Algorithm.java
@@ -15,30 +15,34 @@ public class Algorithm {
     }
 
     public QAP simulatedAnnealing(int maxIterations, double temperature, double temperatureVariation ){
-        Random rnd = new Random();
-        int size = qap.getData().getSize();
 
-        QAP bestSolution = qap.clone(), currentSolution = qap.clone();
+        Random random = new Random();
+        int size = qap.getSize();
+        QAP bestSolution = qap.clone(), currentSolution = qap.clone(), solutionToTest;
+        int indexToPermute2, indexToPermute1;
+        int solutionToTestSum, currentSolutionSum;
+        double probabilityIfWorse;
+
         for(int i=0; i<maxIterations; i++){
-            int indexToPermute1 = rnd.nextInt(size);
-            int indexToPermute2;
-            do{
-                indexToPermute2 = rnd.nextInt(size);
-            }while(indexToPermute2==indexToPermute1);
 
-            QAP solutionToTest = currentSolution.clone();
+            indexToPermute1 = random.nextInt(size);
+            do
+                indexToPermute2 = random.nextInt(size);
+            while (indexToPermute2 == indexToPermute1);
+            solutionToTest = currentSolution.clone();
             solutionToTest.permute(indexToPermute1, indexToPermute2);
-            int solutionToTestSum = solutionToTest.getSum(), currentSolutionSum = currentSolution.getSum();
-            double probabilityIfWorse = (currentSolutionSum - solutionToTestSum )/temperature;
-            System.out.println(probabilityIfWorse);
-            if(solutionToTestSum < currentSolutionSum || rnd.nextDouble() <= probabilityIfWorse){
-                if(solutionToTestSum < bestSolution.getSum()){
-                    bestSolution = solutionToTest.clone();
-                }
-                currentSolution = solutionToTest.clone();
 
+            solutionToTestSum = solutionToTest.sum();
+            currentSolutionSum = currentSolution.sum();
+            probabilityIfWorse = (currentSolutionSum - solutionToTestSum)/temperature;
+            if(solutionToTestSum < currentSolutionSum || random.nextDouble() <= probabilityIfWorse) {
+                if(solutionToTestSum < bestSolution.sum())
+                    bestSolution = solutionToTest.clone();
+
+                currentSolution = solutionToTest.clone();
                 temperature *= temperatureVariation;
             }
+            //System.out.println(probabilityIfWorse);
         }
 
         return bestSolution;
--- src/src/QAP.java
+++ src/src/QAP.java
@@ -1,58 +1,109 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class QAP {
 
-    private Map<Integer, Integer> map;
-    private Data data;
+    private Map<Integer, Integer> locationWithFacility;
+    private int[][] distances;
+    private int[][] weights;
+    private int size;
 
-    public QAP(String path) throws IOException {
+    public QAP(String filePath) throws IOException {
 
-        data = new Data(path);
-        map = new HashMap<>();
-        for (int i = 0; i < data.getSize(); i++)
-            map.put(i,i);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+        size = Integer.valueOf(removeAlinea(bufferedReader.readLine()));
+
+        bufferedReader.readLine();
+        distances = readTable(bufferedReader, size);
+        bufferedReader.readLine();
+        weights = readTable(bufferedReader, size);
+        locationWithFacility = new HashMap<>();
+        for (int i = 0; i < size; i++)
+            locationWithFacility.put(i,i);
     }
 
-    public QAP(Data data, Map<Integer, Integer> map) {
-        this.data = data;
-        this.map = map;
+    public QAP(Map<Integer, Integer> locationWithFacility, int size, int[][] distances, int[][] weights) {
+
+        this.distances = distances;
+        this.weights = weights;
+        this.size = size;
+        this.locationWithFacility = locationWithFacility;
     }
 
-    public int sum(Map<Integer, Integer> arrangement) {
+    private int[][] readTable(BufferedReader bufferedReader, int size) throws IOException {
+        int[][] table = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            String line = removeAlinea(bufferedReader.readLine());
+            table[i] = Arrays
+                    .stream(line.split(" "))
+                    .filter(s -> s.length() != 0)
+                    .mapToInt(Integer::valueOf)
+                    .toArray();
+        }
+        return table;
+    }
+
+    private String removeAlinea(String line) {
+        int nbEspace = 0;
+        while(line.length() > nbEspace && line.charAt(nbEspace) == ' ')
+            nbEspace++;
+        return line.substring(nbEspace);
+    }
+
+    public int sum() {
 
         int sum = 0;
-        for (Map.Entry<Integer, Integer> entry : arrangement.entrySet())
-            for (int i = 0; i < data.getSize(); i++)
-                sum += data.getWeights()[entry.getKey()][i] * data.getDistances()[entry.getValue()][arrangement.get(i)];
+        for (Map.Entry<Integer, Integer> entry : locationWithFacility.entrySet())
+            for (int i = 0; i < size; i++)
+                sum += weights[entry.getKey()][i] * distances[entry.getValue()][locationWithFacility.get(i)];
 
         return sum;
     }
 
-    public int getSum(){
-        return sum(map);
-    }
-
     public void permute(int i, int j) {
 
-        int temp = map.get(i);
-        map.put(i,map.get(j));
-        map.put(j,temp);
+        int temp = locationWithFacility.get(i);
+        locationWithFacility.put(i, locationWithFacility.get(j));
+        locationWithFacility.put(j,temp);
     }
 
-    public Data getData(){
-        return data;
+    public int getSize() {
+        return size;
+    }
+
+    public Map<Integer, Integer> getLocationWithFacility() {
+        return locationWithFacility;
     }
 
     public String toString() {
+
+        final StringBuilder distances = new StringBuilder();
+        final StringBuilder weights = new StringBuilder();
+
+        for(int i = 0; i < size; i++) {
+            distances.append("[ ");
+            weights.append("[ ");
+            for (int j = 0; j < size; j++){
+                distances.append(this.distances[i][j]).append(" ");
+                weights.append(this.weights[i][j]).append(" ");
+            }
+            distances.append("]\n");
+            weights.append("]\n");
+        }
+
         return "QAP {" + "\n" +
-                "map : " + map.toString() + "\n" +
-                "data : " + data.toString() +
+                "size :" + this.size + "\n" +
+                "locationWithFacility : " + locationWithFacility.toString() + "\n" +
+                "distances :\n" + distances.toString() +
+                "weights :\n" + weights.toString() +
                 '}';
     }
 
     public QAP clone() {
-        return new QAP(data.clone(), new HashMap<>(map));
+        return new QAP(new HashMap<>(locationWithFacility), size, Arrays.copyOf(distances, size), Arrays.copyOf(weights, size));
     }
 }
