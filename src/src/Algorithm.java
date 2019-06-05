7779bdbc8fd1e8a9e015d48e64a0230abce1b838
hugoMarguet <hugo.marguet@hotmail.fr>
2019-06-05 09:40:18 +0200

    update Tabou

--- src/src/Algorithm.java
+++ src/src/Algorithm.java
@@ -9,18 +9,16 @@ public class Algorithm {
     private final QAP qap;
     private final int size;
 
-    public Algorithm(QAP qap) {
+    public Algorithm(QAP qap) throws Exception {
+        if (qap.getSize() <= 1)
+            throw new Exception("Size should be higher than 1");
         this.qap = qap;
         this.size = qap.getSize();
     }
 
-    public QAP aleaWalk() {
-        QAP qap = this.qap.clone();
-
-        return qap;
-    }
-
     /**
+     * Neighbourhood' size equals to QAP' size
+     * Temporel complexity : O(nbIterations)
      * @return solution when simulated Annealing
      */
     public QAP simulatedAnnealing(int nbIterations, double temperature, double temperatureVariation) {
@@ -56,25 +54,27 @@ public class Algorithm {
     }
 
     /**
+     * Temporel complexity : O(maxIterations * size²)
+     * @param neighbourhoodSize
      * @param Tsize should be lower than the sum of integers from 0 to QAP' size - 1  to be useful
-     * @param maxIterations to avoid infinite loop
+     * @param maxIterations use to avoid infinite loop
      * @return Tabou algorithm's solution
      */
-    public QAP tabou(int Tsize, int maxIterations) {
+    public QAP tabou(int Tsize, int maxIterations, int neighbourhoodSize) {
         List<Pair<Integer, Integer>> T = new ArrayList<>();
         QAP tabooSolution = qap.clone(), currentSolution = tabooSolution.clone(), solutionToTest = currentSolution.clone();
 
         int solutionToTestSum;
         Pair<Integer, Integer> transition = null;
-        Integer bestLocalSolution;
+        int bestLocalSolution;
         int compteur = 0;
         while (!(new Pair<>(0,0)).equals(transition) && compteur < maxIterations) {
             compteur++;
             bestLocalSolution = Integer.MAX_VALUE;
             transition = new Pair<>(0,0);
             for (int i = 0; i < size; i++) {
-                for (int j = i + 1; j < size; j++) {
-                    if (i != j && !T.contains(new Pair<>(i, j))) {
+                for (int j : neighbourhood(neighbourhoodSize,i)) {
+                    if (!T.contains(new Pair<>(i, j))) {
                         solutionToTest.permute(i, j);
                         solutionToTestSum = solutionToTest.sum();
                         if (bestLocalSolution > solutionToTestSum) {
@@ -95,8 +95,27 @@ public class Algorithm {
                 tabooSolution = currentSolution.clone();
         }
 
-        System.out.println("Compteur : " + compteur + "/" + maxIterations + " -- T size: " + T.size() + "/" + size * (size - 1) / 2);
+        //System.out.println("Compteur : " + compteur + "/" + maxIterations + " -- T size : " + T.size() + "/" + Tsize);
         return tabooSolution;
+    }
+
+    private int[] neighbourhood(int size, int neighbour) {
+
+        final int QAPsize = qap.getSize();
+        int [] neighbours = new int[size];
+        for (int i = 0; i < size; i++)
+            neighbours[i] = i % 2 == 0 ?
+                    neighbour + i >= QAPsize ?
+                            neighbour + i - QAPsize
+                            : neighbour + i
+                    : neighbour - i < 0 ?
+                            neighbour - i + QAPsize
+                            : neighbour - i;
+        return neighbours;
+    }
+
+    public QAP getQap() {
+        return qap;
     }
 
 }
--- src/src/Main.java
+++ src/src/Main.java
@@ -1,38 +1,66 @@
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
 
 public class Main {
 
     private final static String relativePath = "data/";
 
     public static void main (String[] args){
-        try {
 
-            Map<Integer, QAP> QAPs = new HashMap<>();
+
+        try {
+            Map<Integer, Algorithm> QAPs = new HashMap<>();
             int[] sizes = {12, 15, 17, 20, 25, 30, 35, 40, 50, 60, 80, 100};
+            QAP qap;
             for(int size : sizes) {
-                QAPs.put(size,new QAP(relativePath + "tai" + size + "a.txt"));
+                QAPs.put(size,new Algorithm((new QAP(relativePath + "tai" + size + "a.txt"))));
             }
 
 
-            System.out.println("---------------------------------------");
+            qap = QAPs.get(12).getQap();
+            System.out.println("-------------------------------------------------------------------------------------\n");
+            System.out.println("******* QAP 12 intial value : " + qap.sum() +  " -- Tsize max : " + integerSum(qap.getSize() - 1));
+            testTabou(new int[]{8}, QAPs.get(12), 12);
+            System.out.println("Similated Annealing : " + QAPs.get(12).simulatedAnnealing(100,qap.sum(), 0.9).sum());
 
-            //TODO: tester l'algorithme avec des meilleures valeurs
-            QAP simulatedAnnealing = new Algorithm(QAPs.get(12)).simulatedAnnealing(100, QAPs.get(12).sum(), 0.9);
-            System.out.println("QAP found with simulated annealing:");
-            //System.out.println(simulatedAnnealing.toString());
-            System.out.println("QAP sum: " + simulatedAnnealing.sum());
 
-            System.out.println("---------------------------------------");
-            QAP tabou = new Algorithm(QAPs.get(12)).tabou(25, 100000);
-            System.out.println("QAP found with method tabou: ");
-            //System.out.println(simulatedAnnealing.toString());
-            System.out.println("QAP sum: " + tabou.sum());
-            System.out.println("---------------------------------------");
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static int integerSum(int n) {
+        return n * (n + 1) / 2;
+    }
+
+    private static void testTabou(int[] Tsizes, Algorithm QAPAlgorithm, int neighbourhoodSize) {
+
+        final int size = QAPAlgorithm.getQap().getSize();
+        for (int Tsize : Tsizes) {
+            System.out.println("Tabou : " + QAPAlgorithm.tabou(Tsize, 1000, neighbourhoodSize).sum() + "\n-------");
+        }
+    }
+
+    /**
+     *
+     * @param QAPAlgorithm
+     * @param maxIterations
+     * @return
+     */
+    private static int findOptimalTSize(Algorithm QAPAlgorithm, int maxIterations, int neighbourhoodSize) {
+        final int size = QAPAlgorithm.getQap().getSize();
+        final int TsizeMax = size * (size - 1) / 2;
+        int TsizeBest = 0, TsizeBestValue = Integer.MAX_VALUE, sum;
+
+        for (int Tsize = 1; Tsize <= TsizeMax; Tsize++) {
+            if ((sum = QAPAlgorithm.tabou(Tsize, 1000, neighbourhoodSize).sum()) < TsizeBestValue) {
+                TsizeBestValue = sum;
+                TsizeBest = Tsize;
+
+            }
+        }
+        return TsizeBest;
     }
 }
